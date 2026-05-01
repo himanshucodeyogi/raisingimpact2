3fd4c00a65339fcdecb5a1f29fcd7ef938dcbaf4
himanshu <himanshu.codeyogi@gmail.com>
2026-05-01 20:34:14 +0530

    add banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+const site =
+  process.env.SITE_URL ||
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://raisingimpact.org');
+
 export default defineConfig({
-  site: 'https://raisingimpact.org',
+  site,
 });
