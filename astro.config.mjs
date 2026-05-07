b0899146f83ada32ceb0e390a80210d14b19adac
himanshu <himanshu.codeyogi@gmail.com>
2026-05-07 21:02:16 +0530

    new version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-const site =
-  process.env.SITE_URL ||
-  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://raisingimpact.org');
-
 export default defineConfig({
-  site,
+  site: process.env.SITE_URL || 'https://raisingimpact.org',
 });
